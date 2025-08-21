997daaae9c6cb5c886efd8307ef8c6de77bad2f9
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-08-21 15:13:27 +0300

    Add vim SLIME

---
+++ .config/nvim/lua/plugins/slime.lua
@@ -0,0 +1,12 @@
+return {
+  "jpalardy/vim-slime",
+  event = "VeryLazy",
+  init = function()
+    vim.g.slime_target = "tmux"
+    vim.g.slime_bracketed_paste = 1
+    vim.g.slime_default_config = {
+      socket_name = "default",
+      target_pane = "{last}"
+    }
+  end
+}
