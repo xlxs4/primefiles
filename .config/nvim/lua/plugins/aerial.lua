4c0e6407b3faf0bce515185615541dc0b9cefa41
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-09-04 12:19:06 +0300

    Bring aerial back for lualine + fzf integration

---
+++ .config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,11 @@
+return {
+  'stevearc/aerial.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons'
+  },
+  opts = {},
+  keys = {
+    { "<leader>at", "<cmd>AerialToggle<CR>", desc = "Aerial Toggle" },
+  }
+}
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,16 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' }
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    theme = "modus-vivendi",
+    sections = {
+      lualine_x = { "aerial" },
+    },
+    extensions = {
+      "aerial",
+      "man",
+      "nvim-tree",
+      "oil"
+    }
+  }
 }
