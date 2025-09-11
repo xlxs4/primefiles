3f30bcb4dd91845b699799183739c5349833d939
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-09-11 17:46:32 +0300

    Correct treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,28 +2,26 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   branch = "main",
-  version = false,
   build = ':TSUpdate',
-  opts = {
-    -- ensure_installed = {
-    --   "c",
-    --   "lua",
-    --   "vim",
-    --   "vimdoc",
-    --   "markdown",
-    --   "markdown_inline",
-    --   "diff",
-    --   "julia",
-    --   "python",
-    -- },
-    auto_install = false,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    incremental_selection = {
-      enable = true,
-    },
-    indent = { enable = true },
-  },
+  opts = function()
+    require("nvim-treesitter").install({
+      "julia",
+      "python",
+      "rust",
+    })
+  end,
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "julia",
+        "python",
+        "rust",
+      },
+      callback = function()
+        vim.treesitter.start()
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
