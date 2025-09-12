ad953dcf2ff81f4107f6cf1701c63bead8ddaf3b
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-09-12 13:14:57 +0300

    Add vim-tmux-navigator

---
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,18 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigateProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<CR>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<CR>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<CR>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<CR>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>" },
+  }
+}
