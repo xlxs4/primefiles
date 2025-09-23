6f942059fd55555c9da77c369b415ccdc36ec5ff
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-09-23 17:35:20 +0300

    vim: fix auto-session save keybind

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -10,7 +10,7 @@ return {
   keys = {
     { "<leader>wr", "<cmd>AutoSession restore<CR>",       desc = "Session Restore" },
     { "<leader>wf", "<cmd>AutoSession search<CR>",        desc = "Session Search" },
-    { "<leader>ws", "<cmd>AutoSession search<CR>",        desc = "Session Save" },
+    { "<leader>ws", "<cmd>AutoSession save<CR>",          desc = "Session Save" },
     { "<leader>wt", "<cmd>AutoSession toggle<CR>",        desc = "Session Toggle" },
     { "<leader>wd", "<cmd>AutoSession deletePicker<CR>",  desc = "Session Search (Delete)" },
     { "<leader>wp", "<cmd>AutoSession purgeOrphaned<CR>", desc = "Session Purge Orphaned" },
