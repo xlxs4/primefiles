ac3fadbc62664720660121d421db017dcaeb311f
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-09-29 22:59:27 +0300

    vim: Add HJKL navigation inside nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,18 +1,3 @@
-local function open_win_config_func()
-  local scr_w = vim.opt.columns:get()
-  local scr_h = vim.opt.lines:get()
-  local tree_w = 40
-  local tree_h = math.floor(tree_w * scr_h / scr_w)
-  return {
-    border = "double",
-    relative = "editor",
-    width = tree_w,
-    height = tree_h,
-    col = (scr_w - tree_w) / 2,
-    row = (scr_h - tree_h) / 2
-  }
-end
-
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = {
@@ -24,9 +9,61 @@ return {
     view = {
       float = {
         enable = true,
-        open_win_config = open_win_config_func,
+        open_win_config = function()
+          local screen_w = vim.opt.columns:get()
+          local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+          local window_w = screen_w * 0.5
+          local window_h = screen_h * 0.8
+          local window_w_int = math.floor(window_w)
+          local window_h_int = math.floor(window_h)
+          local center_x = (screen_w - window_w) / 2
+          local center_y = ((vim.opt.lines:get() - window_h) / 2)
+              - vim.opt.cmdheight:get()
+          return {
+            border = 'rounded',
+            relative = 'editor',
+            row = center_y,
+            col = center_x,
+            width = window_w_int,
+            height = window_h_int,
+          }
+        end
       }
-    }
+    },
+    on_attach = function(bufnr)
+      local api = require("nvim-tree.api")
+
+      local function edit_or_open()
+        local node = api.tree.get_node_under_cursor()
+        if node.nodes ~= nil then
+          api.node.open.edit()
+        else
+          api.node.open.edit()
+          api.tree.close()
+        end
+      end
+
+      local function vsplit_preview()
+        local node = api.tree.get_node_under_cursor()
+        if node.nodes ~= nil then
+          api.node.open.edit()
+        else
+          api.node.open.vertical()
+        end
+        api.tree.focus()
+      end
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      api.config.mappings.default_on_attach(bufnr)
+
+      vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+      vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+      vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+      vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+    end,
   },
   keys = {
     { "<leader>tt", "<cmd>NvimTreeToggle<CR>",   desc = "Tree Toggle" },
