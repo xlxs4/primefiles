589aec741b260a07a4f49257ba415905f94de151
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-09-29 22:35:38 +0300

    vim: Make nvim-tree floating

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,3 +1,18 @@
+local function open_win_config_func()
+  local scr_w = vim.opt.columns:get()
+  local scr_h = vim.opt.lines:get()
+  local tree_w = 40
+  local tree_h = math.floor(tree_w * scr_h / scr_w)
+  return {
+    border = "double",
+    relative = "editor",
+    width = tree_w,
+    height = tree_h,
+    col = (scr_w - tree_w) / 2,
+    row = (scr_h - tree_h) / 2
+  }
+end
+
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = {
@@ -6,22 +21,15 @@ return {
   version = "*",
   lazy = false,
   opts = {
-    renderer = {
-      icons = {
-        show = {
-          file = false,
-          folder = false,
-          folder_arrow = false,
-          git = false,
-          modified = false,
-          diagnostics = false,
-          bookmarks = false,
-        },
-      },
-    },
+    view = {
+      float = {
+        enable = true,
+        open_win_config = open_win_config_func,
+      }
+    }
   },
   keys = {
-    { "<leader>tt", "<cmd>NvimTreeToggle<CR>", desc = "Tree Toggle" },
+    { "<leader>tt", "<cmd>NvimTreeToggle<CR>",   desc = "Tree Toggle" },
     { "<leader>tf", "<cmd>NvimTreeFindFile<CR>", desc = "Tree Find File" },
     { "<leader>tc", "<cmd>NvimTreeCollapse<CR>", desc = "Tree Collapse" },
   }
