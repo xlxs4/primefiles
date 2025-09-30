60ae06379f53e7a1b12d95661fd5f31fb36b5359
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-09-30 12:38:19 +0300

    vim: Move tree to side

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -19,7 +19,7 @@ return {
         end
       end,
     })
-    -- Automatically resize the floating window when Neovim window size changes
+    -- Automatically resize the tree when Neovim window size changes
     vim.api.nvim_create_augroup("NvimTreeResize", {
       clear = true,
     })
@@ -36,31 +36,6 @@ return {
     require("nvim-tree").setup(opts)
   end,
   opts = {
-    view = {
-      float = {
-        enable = true,
-        -- Move floating tree to center, make it prettier
-        open_win_config = function()
-          local screen_w = vim.opt.columns:get()
-          local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-          local window_w = screen_w * 0.5
-          local window_h = screen_h * 0.8
-          local window_w_int = math.floor(window_w)
-          local window_h_int = math.floor(window_h)
-          local center_x = (screen_w - window_w) / 2
-          local center_y = ((vim.opt.lines:get() - window_h) / 2)
-              - vim.opt.cmdheight:get()
-          return {
-            border = 'rounded',
-            relative = 'editor',
-            row = center_y,
-            col = center_x,
-            width = window_w_int,
-            height = window_h_int,
-          }
-        end
-      }
-    },
     on_attach = function(bufnr)
       local api = require("nvim-tree.api")
 
