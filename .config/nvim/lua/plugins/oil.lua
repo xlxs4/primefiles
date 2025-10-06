3e26e2ac8c70927c0ddf705e37976b88e7ae5445
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2025-10-06 12:33:10 +0300

    vim: Remove indent snack, bring oil back

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,19 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  lazy = false,
+  opts = {
+    columns = {},
+    view_options = {
+      show_hidden = true,
+    },
+    skip_confirm_for_simple_edits = true,
+  },
+  keys = {
+    { "<leader>O", "<cmd>Oil<CR>", desc = "oil" },
+  },
+}
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -100,7 +100,6 @@ return {
     explorer = {
       replace_netrw = true
     },
-    indent = {},
     input = {},
     layout = {},
     gitbrowse = {},
