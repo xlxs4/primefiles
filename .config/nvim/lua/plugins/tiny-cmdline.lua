691192061f6a44eadcbce9e5d3fc00bb0e72528e
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2026-04-17 15:19:51 +0300

    vim: tiny-cmdline keep search centered instead of bottom

--- .config/nvim/lua/plugins/tiny-cmdline.lua
+++ .config/nvim/lua/plugins/tiny-cmdline.lua
@@ -1,5 +1,9 @@
 return {
     "rachartier/tiny-cmdline.nvim",
+    -- Also have search ('/', '?') appear in the center
+    opts = {
+        native_types = {}
+    },
     init = function()
         vim.o.cmdheight = 0
     end,
