07782842b75efd0fc6b35c10c8c1530a884e6ed1
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2026-05-07 23:02:02 +0300

    vim: Browse git hunks from gitsigns through fzf-lua

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,47 @@
+local function quickfix_fzf()
+    require "gitsigns".setqflist("all", { open = false }, function()
+        require "fzf-lua".quickfix()
+    end)
+end
+
+local function quickfix_attached_fzf()
+    require "gitsigns".setqflist("attached", { open = false }, function()
+        require "fzf-lua".quickfix()
+    end)
+end
+
+local function loclist_fzf()
+    -- gitsigns also offers setloclist, but that doesn't have an `open?` option for some reason,
+    -- so we use setqflist. We pipe the current buffer hunks to the location list,
+    -- and the attached/project wide hunks to the quickfix list, because the loclist is
+    -- better suited for buffer/window-specific content, while the quickfix list is better
+    -- suited for data spanning across many files
+    require "gitsigns".setqflist(0, { open = false, use_location_list = true }, function()
+        require "fzf-lua".loclist()
+    end)
+end
+
 return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     keys = {
         {
             "<leader>jl",
+            loclist_fzf,
+            desc = "Browse Hunks in Loclist (Buffer)"
+        },
+        {
+            "<leader>jq",
+            quickfix_fzf,
+            desc = "Browse Hunks in Quickfix (Project)"
+        },
+        {
+            "<leader>ja",
+            quickfix_attached_fzf,
+            desc = "Browse Hunks in Quickfix (Attached)"
+        },
+        {
+            "<leader>ji",
             "<cmd>Gitsigns blame_line<CR>",
             desc = "Show Git Blame for Line"
         },
